21298019504b802580068255f711406695ae0d5b
Tetsuya Idota <tidota@hawaii.edu>
2021-03-28 13:56:06 -1000

    Add eval code

--- eval.cpp
+++ eval.cpp
@@ -42,6 +42,78 @@ void insert_submap(
   }
 }
 
+void eval_map(
+  const std::shared_ptr<octomap::OcTree>& map2eval,
+  const std::shared_ptr<octomap::OcTree>& map_gt)
+{
+  double probHit = map2eval->getProbHit();
+  double probMiss = map2eval->getProbMiss();
+  double resol = map2eval->getResolution();
+  double minx, miny, minz;
+  map2eval->getMetricMin(minx, miny, minz);
+  double maxx, maxy, maxz;
+  map2eval->getMetricMax(maxx, maxy, maxz);
+
+  int count_total = 0;
+  int count_error = 0;
+  int count_false_pos = 0;
+  int count_false_neg = 0;
+  int count_unknown = 0;
+  for (double x = minx - resol/2.0; x <= maxx + resol; x += resol)
+  {
+    for (double y = miny - resol/2.0; y <= maxy + resol; y += resol)
+    {
+      for (double z = minz - resol/2.0; z <= maxz + resol; z += resol)
+      {
+        octomap::point3d query = octomap::point3d(x, y, z);
+        octomap::OcTreeNode* res2check = map2eval->search(query);
+        if (res2check)
+        {
+          ++count_total;
+          octomap::OcTreeNode* res_ans = map_gt->search(query);
+          if (res_ans)
+          {
+            double occ2check = res2check->getOccupancy();
+            double occ_ans = res_ans->getOccupancy();
+            if (occ2check >= probHit && occ_ans <= probMiss)
+            {
+              ++count_false_pos;
+              ++count_error;
+            }
+            else if (occ2check <= probMiss && occ_ans >= probHit)
+            {
+              ++count_false_neg;
+              ++count_error;
+            }
+          }
+          else
+          {
+            ++count_unknown;
+            ++count_error;
+          }
+        }
+      }
+    }
+  }
+  if (count_total > 0)
+  {
+    std::cout << "Total nodes: " << count_total << std::endl;
+    std::cout << "Erroneous nodes: " << count_error << std::endl;
+    std::cout << " - False positive nodes: "
+              << count_false_pos << std::endl;
+    std::cout << " - False negative nodes: "
+              << count_false_neg << std::endl;
+    std::cout << " - Unknown nodes: "
+              << count_unknown << std::endl;
+    std::cout << "Error rate: "
+              << ((double)count_error/count_total) << std::endl;
+  }
+  else
+  {
+    std::cout << "Error: no occ/free node in the map to check." << std::endl;
+  }
+}
+
 int main(int argc, char** argv )
 {
   std::ifstream fin("settings.yaml");
@@ -79,24 +151,27 @@ int main(int argc, char** argv )
   double probHit = submaps_ground_truth[0]->getProbHit();
   double probMiss = submaps_ground_truth[0]->getProbMiss();
   double resol = submaps_ground_truth[0]->getResolution();
+  std::shared_ptr<octomap::OcTree> map_gt
+    = std::make_shared<octomap::OcTree>(resol);
   {
-    std::shared_ptr<octomap::OcTree> map
-      = std::make_shared<octomap::OcTree>(resol);
-
-    map->setProbHit(probHit);
-    map->setProbMiss(probMiss);
-    map->setClampingThresMin(0.12);
-    map->setClampingThresMax(0.97);
+    map_gt->setProbHit(probHit);
+    map_gt->setProbMiss(probMiss);
+    map_gt->setClampingThresMin(0.12);
+    map_gt->setClampingThresMax(0.97);
 
     std::cout << "combining submaps into a ground truth map" << std::flush;
     for (auto m: submaps_ground_truth)
     {
       std::cout << "." << std::flush;
-      insert_submap(m, map);
+      insert_submap(m, map_gt);
     }
     std::cout << " done" << std::endl;
+    std::cout << "compressing..." << std::flush;
+    map_gt->toMaxLikelihood();
+    map_gt->prune();
+    std::cout << " done" << std::endl;
     std::cout << "saving the map..." << std::flush;
-    map->writeBinary("ground_truth_combined.bt");
+    map_gt->writeBinary("ground_truth_combined.bt");
     std::cout << " done" << std::endl;
   }
 
@@ -121,6 +196,12 @@ int main(int argc, char** argv )
       insert_submap(m, map);
     }
     std::cout << " done" << std::endl;
+    std::cout << "compressing..." << std::flush;
+    map->toMaxLikelihood();
+    map->prune();
+    std::cout << " done" << std::endl;
+    std::cout << "evaluating..." << std::endl;
+    eval_map(map, map_gt);
     std::cout << "saving the map..." << std::flush;
     map->writeBinary(map_name + "_combined.bt");
     std::cout << " done" << std::endl;
